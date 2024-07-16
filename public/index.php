85a34d9a51e525a7652e0ae60bb9a79f1de7f9ec
Thebiz <funthebiz@gmail.com>
2024-07-16 08:05:23 +0700

    After section 93 done!

--- public/index.php
+++ public/index.php
@@ -2,18 +2,24 @@
 
 require '../helpers.php';
 
+require basePath('Router.php');
+require basePath('Database.php');
+
+
 // inspect($uri);
 // inspect($method);
 // string(26) "/workopia/public/index.php"
 // string(3) "GET"
 
-require basePath('Router.php');
-
+// Instatiating the router
 $router = new Router();
 
+// Get routes
 $routes = require basePath('routes.php');
 
+// Get current URI and GTTP method
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
+//Route the requrest
 $router->route($uri, $method);
